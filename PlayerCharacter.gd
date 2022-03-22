ff1f252140463013fc1b4e72a0f73776dcdd1c75
Mikko <mikkotakala@hotmail.com>
2022-03-22 17:35:19 +0200

    Player can jump and movement has slowdown

--- PlayerCharacter.gd
+++ PlayerCharacter.gd
@@ -6,6 +6,11 @@ extends KinematicBody2D
 # var b = "text"
 export (int) var speed = 200
 
+var walk_speed = 35
+var jump_speed = -1000
+var gravity = 2500
+var slowdown = 1000
+
 var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
@@ -18,17 +23,28 @@ func _ready():
 #	pass
 
 func get_input():
-	velocity = Vector2()
 	if(Input.is_action_pressed("MoveRight")):
-		velocity.x += 1
+		velocity.x += walk_speed
 	if(Input.is_action_pressed("MoveLeft")):
-		velocity.x -= 1
-	if(Input.is_action_pressed("MoveDown")):
-		velocity.y += 1
+		velocity.x -= walk_speed
+	#if(Input.is_action_pressed("MoveDown")):
+		#velocity.y += 1
 	if(Input.is_action_pressed("MoveUp")):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
+		#velocity.y -= 1
+		if is_on_floor():
+			print("Was on floor")
+			velocity.y = jump_speed
+	#velocity = velocity.normalized() * speed
 	
 func _physics_process(delta):
+	velocity.y += gravity * delta
+	if velocity.x < 0:
+		velocity.x += slowdown * delta
+		if velocity.x > 0:
+			velocity.x = 0
+	elif velocity.x > 0:
+		velocity.x -= slowdown * delta
+		if velocity.x < 0:
+			velocity.x = 0
 	get_input()
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity, Vector2(0, -1))
